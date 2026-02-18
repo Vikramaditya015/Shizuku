21e7330476c1a93112dd9d7ec83106f05429eab5
Vikramaditya015 <vickyvicky0022ok@gmail.com>
2026-02-18 20:16:14 +0530

    Update ServerStatusViewHolder.kt

--- manager/src/main/java/moe/shizuku/manager/home/ServerStatusViewHolder.kt
+++ manager/src/main/java/moe/shizuku/manager/home/ServerStatusViewHolder.kt
@@ -45,7 +45,8 @@ class ServerStatusViewHolder(private val binding: HomeServerStatusBinding, root:
         } else {
             iconView.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_server_error_24dp))
         }
-        val user = if (isRoot) "root" else if (isSystem) "system" else "adb"
+        //val user = if (isRoot) "root" else if (isSystem) "system" else "adb"
+        val user = status.uid
         val title = if (ok) {
             context.getString(R.string.home_status_service_is_running, context.getString(R.string.app_name))
         } else {
